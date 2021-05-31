03533193810f8b17c2e60578765ba0aefc3f12dd
Zachary P. Brasseaux <zbrasseaux97@gmail.com>
2021-05-31 11:00:30 -0500

    Seperate declarations (#10)
    Co-authored-by: Brady Anderson <bradya48@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include "registers.h"
 
 int main() {
-    std::cout << "Hello, Buzz-GB!" << std::endl;
+    std::cout << "Welcome to Buzz-GB!" << std::endl;
+    Registers gb_registers{};
+
+    gb_registers.set_a(0xab);
+    gb_registers.set_f(0xcd);
+
+    gb_registers.set_b(0xef);
+    gb_registers.set_c(0xab);
+
+    gb_registers.set_de(0x1234);
+
+    gb_registers.set_hl(0x0000);
+
+    gb_registers.set_sp(0x4444);
+    gb_registers.set_pc(0x1000);
+
+
+    gb_registers.dump_registers();
     return 0;
 }
 
