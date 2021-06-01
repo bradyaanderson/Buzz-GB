7218558bc183f56982b0d85e85e87b812c7c2c51
Zachary P. Brasseaux <zbrasseaux97@gmail.com>
2021-05-31 22:02:34 -0500

    Add class-based registers (#9)
    * class-based implementation of accessible registers
    
    * improve comments on registers.h
    
    * register_dump prints hex values of registers
    
    * move dump_registers and reformat block comments
    
    * typo fix
    
    * Seperate declarations (#10)
    
    Co-authored-by: Brady Anderson <bradya48@gmail.com>
    
    * fix for dump_registers
    
    Co-authored-by: Brady Anderson <bradya48@gmail.com>

---
+++ src/registers.cpp
@@ -0,0 +1,143 @@
+#include "registers.h"
+
+constexpr uint8_t Registers::a() const noexcept {
+    return reg_a;
+}
+
+void Registers::set_a(uint8_t value) noexcept {
+    reg_a = value;
+}
+
+constexpr uint8_t Registers::f() const noexcept {
+    return reg_f;
+}
+
+void Registers::set_f(uint8_t value) noexcept {
+    reg_f = value;
+}
+
+constexpr uint16_t Registers::af() const noexcept {
+    return (reg_f << 8) | reg_a;
+}
+
+void Registers::set_af(uint16_t value) noexcept {
+    reg_f = value >> 8;
+    reg_a = value;
+}
+
+constexpr uint8_t Registers::b() const noexcept {
+    return reg_b;
+}
+
+void Registers::set_b(uint8_t value) noexcept {
+    reg_b = value;
+}
+
+constexpr uint8_t Registers::c() const noexcept {
+    return reg_c;
+}
+
+void Registers::set_c(uint8_t value) noexcept {
+    reg_c = value;
+}
+
+constexpr uint16_t Registers::bc() const noexcept {
+    return (reg_c << 8) | reg_b;
+}
+
+void Registers::set_bc(uint16_t value) noexcept {
+    reg_c = value >> 8;
+    reg_b = value;
+}
+
+constexpr uint8_t Registers::d() const noexcept {
+    return reg_d;
+}
+
+void Registers::set_d(uint8_t value) noexcept {
+    reg_d = value;
+}
+
+constexpr uint8_t Registers::e() const noexcept {
+    return reg_e;
+}
+
+void Registers::set_e(uint8_t value) noexcept {
+    reg_e = value;
+}
+
+constexpr uint16_t Registers::de() const noexcept {
+    return (reg_e << 8) | reg_d;
+}
+
+void Registers::set_de(uint16_t value) noexcept {
+    reg_e = value >> 8;
+    reg_d = value;
+}
+
+constexpr uint8_t Registers::h() const noexcept {
+    return reg_h;
+}
+
+void Registers::set_h(uint8_t value) noexcept {
+    reg_h = value;
+}
+
+constexpr uint8_t Registers::l() const noexcept {
+    return reg_l;
+}
+
+void Registers::set_l(uint8_t value) noexcept {
+    reg_l = value;
+}
+
+constexpr uint16_t Registers::hl() const noexcept {
+    return (reg_l << 8) | reg_h;
+}
+
+void Registers::set_hl(uint16_t value) noexcept {
+    reg_l = value >> 8;
+    reg_h = value;
+}
+
+constexpr uint16_t Registers::sp() const noexcept {
+    return reg_sp;
+}
+
+void Registers::set_sp(uint16_t value) noexcept {
+    reg_sp = value;
+}
+
+constexpr uint16_t Registers::pc() const noexcept {
+    return reg_pc;
+}
+
+void Registers::set_pc(uint16_t value) noexcept {
+    reg_pc = value;
+}
+
+/*
+* Basic function to output all the data in all the registers.
+* Primarily for de-bugging purposes.
+*/
+void Registers::dump_registers() {
+    cout
+            << "a : " << hex << (uint32_t) a() << endl
+            << "f : " << hex << (uint32_t) f() << endl
+            << "af: " << hex << (uint32_t) af() << endl
+            << endl
+            << "b : " << hex << (uint32_t) b() << endl
+            << "c : " << hex << (uint32_t) c() << endl
+            << "bc: " << hex << (uint32_t) bc() << endl
+            << endl
+            << "d : " << hex << (uint32_t) d() << endl
+            << "e : " << hex << (uint32_t) e() << endl
+            << "de: " << hex << (uint32_t) de() << endl
+            << endl
+            << "h : " << hex << (uint32_t) h() << endl
+            << "l : " << hex << (uint32_t) l() << endl
+            << "hl: " << hex << (uint32_t) hl() << endl
+            << endl
+            << "sp: " << hex << (uint32_t) sp() << endl
+            << "pc: " << hex << (uint32_t) pc() << endl;
+}
---
+++ src/registers.h
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Header file for register implementation.
+ *
+ * Each block contains two 8-bit registers, then a simulated 16-bit register
+ * created by joining the 8-bit registers together.
+ *
+ * There is a getter and setter for each register. The getters and setters for
+ * the combined registers get and set based on what is contained in the 8-bit 
+ * There are additionally two naturally 16-bit registers, pc and sp.
+ */
+class Registers {
+public:
+    // initialize register variables
+    uint8_t reg_a, reg_b, reg_c, reg_d, reg_e, reg_f, reg_h, reg_l; // 8-bit
+    uint16_t reg_sp, reg_pc;                                        // 16-bit
+
+    // a, f, af
+    [[nodiscard]] constexpr uint8_t a() const noexcept;
+
+    void set_a(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint8_t f() const noexcept;
+
+    void set_f(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint16_t af() const noexcept;
+
+    void set_af(uint16_t value) noexcept;
+
+    // b, c, bc
+    [[nodiscard]] constexpr uint8_t b() const noexcept;
+
+    void set_b(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint8_t c() const noexcept;
+
+    void set_c(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint16_t bc() const noexcept;
+
+    void set_bc(uint16_t value) noexcept;
+
+    // d, e, de
+    [[nodiscard]] constexpr uint8_t d() const noexcept;
+
+    void set_d(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint8_t e() const noexcept;
+
+    void set_e(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint16_t de() const noexcept;
+
+    void set_de(uint16_t value) noexcept;
+
+    // h, l, hl
+    [[nodiscard]] constexpr uint8_t h() const noexcept;
+
+    void set_h(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint8_t l() const noexcept;
+
+    void set_l(uint8_t value) noexcept;
+
+    [[nodiscard]] constexpr uint16_t hl() const noexcept;
+
+    void set_hl(uint16_t value) noexcept;
+
+    // sp, pc
+    [[nodiscard]] constexpr uint16_t sp() const noexcept;
+
+    void set_sp(uint16_t value) noexcept;
+
+    [[nodiscard]] constexpr uint16_t pc() const noexcept;
+
+    void set_pc(uint16_t value) noexcept;
+
+    /*
+    * Basic function to output all the data in all the registers.
+    * Primarily for de-bugging purposes.
+    */
+    void dump_registers(); 
+    
+};
